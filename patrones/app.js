d4d32ac2d5cf547459b696c04ce33c9f6c6c07eb
ruruisu <zaddozgl@gmail.com>
2022-03-06 13:29:37 -0600

    greet3

--- patrones/app.js
+++ patrones/app.js
@@ -1,9 +1,11 @@
 //references to the exported functions
 let greet = require('./greet1');
-//let greet2 = require('./greet2').greet;
+let greet2 = require('./greet2').greet;
 let greet2a = require('./greet2');
-
+const greet3 = require('./greet3');
 
 greet();
-//greet2();
-greet2a.greet() //this effectively does the same thing as the commented code above
+greet2();
+greet2a.greet() //this effectively does the same thing as the code above
+console.log(greet3);
+greet3.greet();
