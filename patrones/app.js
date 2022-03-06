59c81f110197dee6a9a6b6d5f7d91ef7dd7e2fc7
ruruisu <zaddozgl@gmail.com>
2022-03-06 15:11:40 -0600

    greet4

--- patrones/app.js
+++ patrones/app.js
@@ -1,3 +1,20 @@
+//references to the exported functions
+let greet = require('./greet1');
+let greet2 = require('./greet2').greet;
+let greet2a = require('./greet2');
+const{ greeting } = require('./greet3')
+let greet3 = require('./greet3');
+
+greet();
+greet2();
+greet2a.greet() //this effectively does the same thing as the code above
+
+console.log(greet3);
+greet3.greet();
+greet3.greeting = 'Hello from the app'
+
+let greet3b = require('./greet3');
+greet3b.greet();//this demonstrates that greet3 is a reference in memory to the original, not an actual separate value
 
 const Greet4 = require('./greet4');
 let myGreet = new (Greet4)
